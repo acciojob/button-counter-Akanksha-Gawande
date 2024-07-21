88890a651b59d849d5c073cc7b2caaa88f76ebc8
Akanksha-Gawande <akankshagawande17@gmail.com>
2024-07-21 15:48:42 +0530

    abc

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount = count + 1;
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {setCount} Times</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
